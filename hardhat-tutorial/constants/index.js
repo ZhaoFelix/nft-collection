061d9ac9273c90cf12f0f5a269c8790a219b9790
ZhaoFelix <15021179915@163.com>
2022-11-07 20:01:55 +0800

    feat: 更新合约地址

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // 之前白名单的合约地址
 const WHITELIST_CONTRACT_ADDRESS = '0x509a63bff26d2993eb441e7a581fd65f17c6faed'
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = 'https://nft-collection-sneh1999.vercel.app/api/'
+const METADATA_URL = 'https://nft-collection-rho-seven.vercel.app/api/'
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL }
